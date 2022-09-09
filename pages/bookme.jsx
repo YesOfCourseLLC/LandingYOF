a9d46f05bf5267cc39d3d6ecdb4b9ba35568fc94
David Valencia <codingdavid9@outlook.com>
2022-09-08 21:50:51 -0700

    html home

---
+++ pages/bookme.jsx
@@ -0,0 +1,16 @@
+
+import React, { useRef } from 'react'
+
+import Calendar from '../components/Calendar'
+
+class bookme extends React.Component{
+    render(){
+        return (
+            <>
+                <Calendar/>
+            </>
+        )
+    }
+}
+
+export default bookme;
