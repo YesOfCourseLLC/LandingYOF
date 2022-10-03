5500be287ad9ae1d5e671af8462671b7fedf689b
MaidedPonce <maided.h.p90@gmail.com>
2022-10-02 19:18:59 -0500

    adding extension

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='flex h-32 justify-center items-center flex-col bg-primary-blue'>
       <a className='text-white' href='mailto:contact@yeahofcourse.com'>contact@yeahofcourse.com</a>
-      <a className='text-white' href='tel:7257249285'>7257249285</a>
+      <a className='text-white' href='tel:+17257249285'>7257249285</a>
       <span className='text-brand-yellow'>All rights reserved</span>
     </footer>
   )
