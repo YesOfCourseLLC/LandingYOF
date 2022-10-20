429bffac037264e09d347372c8c802def29636af
MaidedPonce <maided.h.p90@gmail.com>
2022-10-19 20:33:01 -0500

    adding bg none

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -5,7 +5,7 @@ import DarkMode from '../Components/DarkMode'
 const Header = ({ locale, enabled, setEnabled }) => {
   return (
     <header className={`${enabled && 'bg-black'} flex justify-between items-center h-24`}>
-      <nav className={`${enabled && 'bg-black shadow-dark-brand'} transition ease-in-out delay-150 fixed flex justify-between w-full bg-white pl-8 items-center dark:bg-black shadow-md h-24 z-10 gap-x-2`}>
+      <nav className={`${enabled && 'bg-black shadow-dark-brand'} transition ease-in-out delay-150 fixed flex justify-between w-full pl-8 items-center dark:bg-black shadow-md h-24 z-10 gap-x-2`}>
         <DarkMode locale={locale} enabled={enabled} setEnabled={setEnabled} />
       </nav>
     </header>
